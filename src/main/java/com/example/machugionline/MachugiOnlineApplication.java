2d7815b9efbc53e3c9874c609ebdc56c7d541590
unknown <geniusbrainlsm@gmail.com>
2024-05-03 12:14:03 +0900

    init

---
+++ src/main/java/com/example/machugionline/MachugiOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.machugionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachugiOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachugiOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/machugionline/MachugiOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.machugionline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachugiOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
